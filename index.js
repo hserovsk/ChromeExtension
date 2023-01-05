fbd82e433a7ffd74be20fb810797b1b1ee69a243
Hubert Serowski R <hserv@protonmail.com>
2023-01-05 13:52:18 +0100

    icon and manifest.json added

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ let myLeads = []
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     renderLeads()
 })
 function renderLeads(){
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++){
-        listItems += "<li>" + myLeads[i] + "</li>"
+        //listItems += "<li><a href=`" + myLeads[i] + "'target='_blank`>" + myLeads[i] + "</a></li>"
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+`
 
     }
     ulEl.innerHTML = listItems
